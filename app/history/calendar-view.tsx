759e36ee8a077ba4ef7213631c0b39ddb088d865
Rewel Cabiles <rewel.cabiles@gmail.com>
2025-03-21 00:09:58 +0800

    update package dependencies and refactor DatePicker and Input components for improved functionality and styling

--- app/history/calendar-view.tsx
+++ app/history/calendar-view.tsx
@@ -2,13 +2,7 @@
 
 import { useState } from "react"
 import { format } from "date-fns"
-import { CalendarIcon } from "lucide-react"
 import { useRouter, useSearchParams } from "next/navigation"
-
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Calendar } from "@/components/ui/calendar"
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
 
 export function DatePicker() {
   const router = useRouter()
@@ -32,20 +26,12 @@ export function DatePicker() {
   }
 
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button
-          variant={"outline"}
-          className={cn("w-full justify-start text-left font-normal", !date && "text-muted-foreground")}
-        >
-          <CalendarIcon className="mr-2 h-4 w-4" />
-          {date ? format(date, "PPP") : <span>Pick a date</span>}
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-auto p-0">
-        <Calendar mode="single" selected={date} onSelect={handleDateSelect} initialFocus />
-      </PopoverContent>
-    </Popover>
+    
+    <>
+      <pre className="text-sm text-gray-400">Select a date:</pre>
+      <input className="border-2 p-3 text-sm rounded-md bg-zinc-950"
+    type="date" value={date ? format(date, "yyyy-MM-dd") : ""} onChange={(e: any) => handleDateSelect(new Date(e.target.value))} />
+    </>
   )
 }
 
