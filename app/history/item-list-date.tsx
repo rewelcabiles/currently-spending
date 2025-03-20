78bbb1ac3beb57e2c56934666f15109474295d93
Rewel Cabiles <rewel.cabiles@gmail.com>
2025-03-21 00:24:37 +0800

    refactor HistoryPage to use Suspense for loading DatePicker and ItemListDate components

--- app/history/item-list-date.tsx
+++ app/history/item-list-date.tsx
@@ -1,7 +1,7 @@
 'use client';
 import ItemList from "@/components/ui/item_list";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 
 export default function ItemListDate() {
     const searchParams = useSearchParams();
--- app/history/page.tsx
+++ app/history/page.tsx
@@ -1,17 +1,20 @@
 'use client'
-import React, { useEffect } from 'react';
+import React, { Suspense } from 'react';
 import { DatePicker } from './calendar-view';
-import ItemList from '@/components/ui/item_list';
-import { useSearchParams } from 'next/navigation';
 import ItemListDate from './item-list-date';
+import LoadingSpinner from '@/components/ui/spinner';
 
 export default function HistoryPage() {
     
     return (
         <div className="flex-1 w-full flex flex-col gap-6 ">
             <h1 className='w-full text-center text-lg font-semibold'>Looking back at your regrets huh?</h1>
-            <DatePicker />
-            <ItemListDate />
+            <Suspense fallback={
+                <LoadingSpinner size='lg'/>
+            }>
+                <DatePicker />
+                <ItemListDate />
+            </Suspense>
         </div>
     );
 };
