b159867a7826de440a748161109d6eaa011f2d02
Rewel Cabiles <rewel.cabiles@gmail.com>
2025-03-20 22:29:11 +0800

    update app icons in manifest and replace deprecated icon files for improved branding

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,16 +11,15 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#000000',
     icons: [
       {
-        src: '/icon.jpg',
+        src: '/web-app-manifest-192x192.png',
         sizes: '192x192',
-        type: 'image/jpg',
+        type: 'image/png',
       },
       {
-        src: '/icon.jpg',
+        src: '/web-app-manifest-512x512.png',
         sizes: '512x512',
-        type: 'image/jpg',
+        type: 'image/png',
       },
     ],
-    
   }
 }
