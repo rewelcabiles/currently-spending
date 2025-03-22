5a1f3c2305fcb8a202e07a35924f370c651f496c
Rewel Cabiles <rewel.cabiles@gmail.com>
2025-03-22 12:46:50 +0800

    Add Category interface to define category structure in spending records

--- utils/interfaces.ts
+++ utils/interfaces.ts
@@ -3,4 +3,14 @@ export interface Spending {
     created_at: string;
     item: string;
     price: number;
+    category_id: Category | null;
+}
+
+export interface Category {
+    id: number;
+    created_at: string;
+    name: string;
+    is_default: boolean;
+    user_id: string;
+    parent_id: Category | null;
 }
